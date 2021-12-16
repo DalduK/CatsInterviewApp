c8feab5ca12a2d386eee3cd1fdd7f801125f06fe
DalduK <przemyslaw7wozny@gmail.com>
2021-12-16 16:30:45 +0100

    Initial Commit

---
+++ DOOKZadanie/DOOKZadanieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOOKZadanieApp.swift
+//  DOOKZadanie
+//
+//  Created by Przemysław Woźny on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct DOOKZadanieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
